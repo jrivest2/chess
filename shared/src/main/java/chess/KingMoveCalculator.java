8194d6f6e23fff3c52ac48a4fe93ebe41d588d68
jrivest2 <justinrivest@gmail.com>
2026-09-19 13:38:52 -0600

    impl(p0) Added KingMoveCalculator

---
+++ shared/src/main/java/chess/KingMoveCalculator.java
@@ -0,0 +1,10 @@
+package chess;
+
+import java.util.List;
+
+public class KingMoveCalculator implements MovesCalculator {
+    @Override
+    public List<ChessPosition> getValidMoves(ChessPosition position, ChessBoard board) {
+        return List.of();
+    }
+}
