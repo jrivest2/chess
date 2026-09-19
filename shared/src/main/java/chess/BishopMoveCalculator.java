2a2a66e5d6b31e124cbf78a6c365bd2c915184aa
jrivest2 <justinrivest@gmail.com>
2026-09-19 13:33:15 -0600

    impl(p0) Refactored BishopMoveCalculator

--- shared/src/main/java/chess/BishopMoveCalculator.java
+++ shared/src/main/java/chess/BishopMoveCalculator.java
@@ -5,6 +5,18 @@ import java.util.List;
 
 public class BishopMoveCalculator implements MovesCalculator{
     private enum Direction {POSITIVE_ROW_POSITIVE_COL, POSITIVE_ROW_NEGATIVE_COL, NEGATIVE_ROW_NEGATIVE_COL, NEGATIVE_ROW_POSITIVE_COL,}
+    private List<ChessPosition> moves = new ArrayList<>();
+    private List<ChessPosition> blacklist = new ArrayList<>();
+    private final int[][] offsets = {
+            {1,1},{1,-1},{-1,-1},{-1,1},
+            {2,2},{2,-2},{-2,-2},{-2,2},
+            {3,3},{3,-3},{-3,-3},{-3,3},
+            {4,4},{4,-4},{-4,-4},{-4,4},
+            {5,5},{5,-5},{-5,-5},{-5,5},
+            {6,6},{6,-6},{-6,-6},{-6,6},
+            {7,7},{7,-7},{-7,-7},{-7,7}
+    };
+
     private Direction findOffsetDirection(int[] offset) {
         if (offset[0] >= 0) {
             if (offset[1] >= 0) return Direction.POSITIVE_ROW_POSITIVE_COL;
@@ -12,19 +24,33 @@ public class BishopMoveCalculator implements MovesCalculator{
         } else if (offset[1] >= 0) return Direction.NEGATIVE_ROW_POSITIVE_COL;
         else return Direction.NEGATIVE_ROW_NEGATIVE_COL;
     }
+    private void updateBlacklist(ChessPosition target, List<ChessPosition> blacklist, Direction offsetDirection) {
+        switch (offsetDirection) {
+            case POSITIVE_ROW_POSITIVE_COL: {
+                for (int i = target.getRow(), j = target.getColumn(); i <= 8 && j <= 8 ; i++, j++) {
+                    blacklist.add(new ChessPosition(i,j));
+                }
+            }
+            case POSITIVE_ROW_NEGATIVE_COL: {
+                for (int i = target.getRow(), j = target.getColumn(); i <= 8 && j >= 1 ; i++, j--) {
+                    blacklist.add(new ChessPosition(i,j));
+                }
+            }
+            case NEGATIVE_ROW_NEGATIVE_COL: {
+                for (int i = target.getRow(), j = target.getColumn(); i >= 1 && j >= 1 ; i--, j--) {
+                    blacklist.add(new ChessPosition(i,j));
+                }
+            }
+            case NEGATIVE_ROW_POSITIVE_COL: {
+                for (int i = target.getRow(), j = target.getColumn(); i >= 1 && j <= 8 ; i--, j++) {
+                    blacklist.add(new ChessPosition(i,j));
+                }
+            }
+        }
+    }
+
     public List<ChessPosition> getValidMoves(ChessPosition position, ChessBoard board) {
-        List<ChessPosition> moves = new ArrayList<>();
         ChessPiece piece = board.getPiece(position);
-        int[][] offsets = {
-                {1,1},{1,-1},{-1,-1},{-1,1},
-                {2,2},{2,-2},{-2,-2},{-2,2},
-                {3,3},{3,-3},{-3,-3},{-3,3},
-                {4,4},{4,-4},{-4,-4},{-4,4},
-                {5,5},{5,-5},{-5,-5},{-5,5},
-                {6,6},{6,-6},{-6,-6},{-6,6},
-                {7,7},{7,-7},{-7,-7},{-7,7}
-        };
-        List<ChessPosition> blacklist = new ArrayList<>();
 
         for (int[] offset: offsets) {
             ChessPosition target = position.addOffset(offset[0], offset[1]);
@@ -35,28 +61,7 @@ public class BishopMoveCalculator implements MovesCalculator{
             } else if (blacklist.contains(target)){ continue;
             } else if (board.getPiece(target) != null) {
                 if (board.getPiece(target).getTeamColor() != piece.getTeamColor()) moves.add(target);
-                switch (offsetDirection) {
-                    case POSITIVE_ROW_POSITIVE_COL: {
-                        for (int i = target.getRow(), j = target.getColumn(); i <= 8 && j <= 8 ; i++, j++) {
-                            blacklist.add(new ChessPosition(i,j));
-                        }
-                    }
-                    case POSITIVE_ROW_NEGATIVE_COL: {
-                        for (int i = target.getRow(), j = target.getColumn(); i <= 8 && j >= 1 ; i++, j--) {
-                            blacklist.add(new ChessPosition(i,j));
-                        }
-                    }
-                    case NEGATIVE_ROW_NEGATIVE_COL: {
-                        for (int i = target.getRow(), j = target.getColumn(); i >= 1 && j >= 1 ; i--, j--) {
-                            blacklist.add(new ChessPosition(i,j));
-                        }
-                    }
-                    case NEGATIVE_ROW_POSITIVE_COL: {
-                        for (int i = target.getRow(), j = target.getColumn(); i >= 1 && j <= 8 ; i--, j++) {
-                            blacklist.add(new ChessPosition(i,j));
-                        }
-                    }
-                }
+                updateBlacklist(target,blacklist,offsetDirection);
             } else {
                 moves.add(target);
             }
