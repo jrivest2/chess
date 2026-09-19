60c605efe13177001940b4e15e2ad4a100fbd0f8
jrivest2 <justinrivest@gmail.com>
2026-09-19 14:48:20 -0600

    impl(p0) Added KnightMoveCalculator

---
+++ shared/src/main/java/chess/KnightMoveCalculator.java
@@ -0,0 +1,10 @@
+package chess;
+
+import java.util.List;
+
+public class KnightMoveCalculator implements MovesCalculator {
+    @Override
+    public List<ChessPosition> getValidMoves(ChessPosition position, ChessBoard board) {
+        return List.of();
+    }
+}
